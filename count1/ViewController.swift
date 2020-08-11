f334ae38ec8a7e926016e9525bb2036ab443fee7
yutooni <yuto.oni1227@gmail.com>
2020-08-11 16:28:47 +0900

    add: plus minus

--- count1/ViewController.swift
+++ count1/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    使用するデータの宣言
+    var number: Int = 0
+//    変数の書き方
+//    var 変数名:型 = 初期値
+    @IBOutlet var label: UILabel!
+//    IB interface builder Outlet プログラムから命令を受け取るパーツ
+//    var 変数名: データの型!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +25,18 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func plus(){
+//        numberに1を足したものを再代入
+        number += 1
+//        labelのテキスト部分にnumberを入れる
+//        int型をstringに変換する
+        label.text = String(number)
+    }
+    
+    @IBAction func minus(){
+        number -= 1
+        label.text = String(number)
+    }
+    
 }
 
